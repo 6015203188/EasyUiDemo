cf043d3349957697e81a377b9e93a69769120c74
lch <nervds@163.com>
2020-07-25 00:18:21 +0800

    delete fix_2

--- src/main/java/com/nervds/Run.java
+++ src/main/java/com/nervds/Run.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Run {
     public static void main(String[] args) {
         SpringApplication.run(Run.class, args);
+        // fix_1
     }
 }
